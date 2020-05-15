575b76900e8c60dca2e21127410947e56868bf44
Brian Cain <brianccain@gmail.com>
2020-05-14 18:07:36 -0700

    Add CameraFollow script

---
+++ CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+  public Transform targetTransform;
+  public float followSpeed = 5f;
+
+  void FixedUpdate() {
+     Vector3 newPosition = targetTransform.position;
+     newPosition.z = -10;
+     transform.position = Vector3.Slerp(transform.position, newPosition, followSpeed * Time.deltaTime);
+  }
+}
